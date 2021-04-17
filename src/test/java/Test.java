d418afc2983e20e0e59afd0f0890b72607e0d55d
Neil <h66r10@gmail.com>
2021-04-17 18:22:12 +0200

    Simple git management

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,15 +4,24 @@ import kaptainwutax.mcutils.nbt.NBTIO;
 import kaptainwutax.mcutils.util.pos.BPos;
 import kaptainwutax.mcutils.version.MCVersion;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 
 public class Test {
 
     public static void main(String[] args) throws IOException {
-        PieceInfo structure = new PieceInfo(MCVersion.v1_16_2).fromTag(NBTIO.read("plains_meeting_point_1.nbt"));
+        URL url= Test.class.getResource("plains_meeting_point_1.nbt");
+        assert url != null;
+        File file=new File(url.getFile());
+        PieceInfo structure = new PieceInfo(MCVersion.v1_16_2).fromTag(NBTIO.read(file));
         structure.getPalette().replace(Block.COBBLESTONE, Block.STONE);
         structure.getBlockIDs().put(new BPos(0, 0, 0), structure.getPalette().indexOf(Block.STONE));
-        NBTIO.write(structure.toTag(), "plains_meeting_point_1_modified.nbt", true);
+        File got = File.createTempFile("plains_meeting_point_1_modified", ".nbt");
+        String outputPath = got.getAbsolutePath();
+        System.out.println("Wrote file to "+outputPath);
+        NBTIO.write(structure.toTag(), outputPath, true);
     }
 
 }
